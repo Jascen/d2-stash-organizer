8049f6ee0b55094be6264c6d362e6187fd98a3e2
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-29 21:24:17 -0400

    -

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -26,7 +26,7 @@ function App() {
     <div id="app" class={accessibleFont ? "accessible-font" : ""}>
       <GitHubLink />
       <HelpLink />
-      <h1>Project Diablo 2 Collection Manager (DEBUG)</h1>
+      <h1>Project Diablo 2 Collection Manager (PlugY Season 12)</h1>
       <Routes />
     </div>
   );
