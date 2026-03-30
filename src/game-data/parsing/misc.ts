d475a7d19c9a62a3150de8019c71e177878ac4b5
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-29 21:11:29 -0400

    - S12: Adjust for newly added columns to MISC txt

--- src/game-data/parsing/misc.ts
+++ src/game-data/parsing/misc.ts
@@ -5,19 +5,19 @@ import { getString } from "../strings";
 export async function miscToJson() {
   const misc: Record<string, Misc> = {};
   for (const line of await readGameFile("Misc")) {
-    const code = line[13].trim();
+    const code = line[14].trim();
     misc[code] = {
-      name: getString(line[15].trim()),
-      type: line[32].trim(),
+      name: getString(line[16].trim()),
+      type: line[33].trim(),
       tier: 0,
-      maxSockets: Number(line[20]),
+      maxSockets: Number(line[21]),
       indestructible: line[10].trim() === "1",
-      width: Number(line[17]),
-      height: Number(line[18]),
+      width: Number(line[18]),
+      height: Number(line[19]),
       qlevel: Number(line[5]),
       levelReq: Number(line[6]),
-      stackable: line[43] === "1",
-      trackQuestDifficulty: line[48] === "1" || undefined,
+      stackable: line[44] === "1",
+      trackQuestDifficulty: line[49] === "1" || undefined,
     };
     // Token of absolution name is messed up, has the description at the start
     if (code === "toa") {
